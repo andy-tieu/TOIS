0bc4ce67eeba8d250a8b9618442e13831bb1cefa
Ann-Dinh <147282110+Ann-Dinh@users.noreply.github.com>
2023-10-08 10:56:45 +0000

    added html, css, js for game timer

---
+++ public/app2.js
@@ -0,0 +1,23 @@
+const timeH = document.getElementById('timer');
+let timeSecond = 10;
+
+displayTime(timeSecond) 
+
+const countDown = setInterval (()=>{
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond <= 0 || timeSecond<1){
+        endTime();
+        clearInterval(countDown);
+    }
+},1000)
+
+function displayTime(second){
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timeH.innerHTML= `${min<10 ? '0': ''}${min}:${sec<10 ? '0' :''}${sec}`
+}
+function endTime(){
+    timeH.innerHTML = 'TIME FOR A BREAK!!'
+    location.href ="./public/games.html";
+}
