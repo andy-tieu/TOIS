f59571a308fe9418844ec84cdaa638fa6d80e43a
Ann-Dinh <147282110+Ann-Dinh@users.noreply.github.com>
2023-10-08 07:37:23 +0000

    Added timer

--- script.js
+++ script.js
@@ -22,6 +22,30 @@ const executeCodes = () => {
 //executeCodes function will be called on webpage load
 window.addEventListener("load", executeCodes);
 
+const timeH = document.querySelector('h1');
+let timeSecond = 5;
+
+displayTime(timeSecond) 
+
+const countDown = setInterval (()=>{
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond <= 0 || timeSecond<1){
+        endTime();
+        clearInterval(countDown);
+    }
+},1000)
+
+function displayTime(second){
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timeH.innerHTML= `${min<10 ? '0': ''}${min}:${sec<10 ? '0' :''}${sec}`
+}
+function endTime(){
+    timeH.innerHTML = 'TIME FOR A BREAK!!'
+    window.location="https://sites.google.com/view/unblockgames-hackuta/home";
+}
+
 try {
   // Code that may throw an error
   // For example, accessing an undefined variable or calling a function with incorrect arguments
