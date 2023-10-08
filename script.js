8209a52904b25a443971cc81ddd263af54423ea2
Ann-Dinh <147282110+Ann-Dinh@users.noreply.github.com>
2023-10-08 07:55:49 +0000

    deleted the cookies trial. Add button to new tab

--- script.js
+++ script.js
@@ -1,67 +1,23 @@
-const cookieBox = document.querySelector(".wrapper"),
-  buttons = document.querySelectorAll(".button");
-
-const executeCodes = () => {
-  //if cookie contains codinglab it will be returned and below of this code will not run
-  if (document.cookie.includes("codinglab")) return;
-  cookieBox.classList.add("show");
-
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      cookieBox.classList.remove("show");
-
-      //if button has acceptBtn id
-      if (button.id == "acceptBtn") {
-        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
-        document.cookie = "cookieBy= codinglab; max-age=" + 60 * 60 * 24 * 30;
-      }
-    });
-  });
-};
-
-//executeCodes function will be called on webpage load
-window.addEventListener("load", executeCodes);
-
 const timeH = document.querySelector('h1');
-let timeSecond = 5;
+let timeSecond = 1200;
 
-// Call displayTime initially to set the initial time
-displayTime(timeSecond);
+displayTime(timeSecond) 
 
-// Create a function to start the countdown timer
-function startCountdown() {
-  const countDown = setInterval(() => {
+const countDown = setInterval (()=>{
     timeSecond--;
     displayTime(timeSecond);
-
-    if (timeSecond <= 0) {
-      clearInterval(countDown);
-      endTime();
+    if(timeSecond <= 0 || timeSecond<1){
+        endTime();
+        clearInterval(countDown);
     }
-  }, 1000);
+},1000)
+
+function displayTime(second){
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timeH.innerHTML= `${min<10 ? '0': ''}${min}:${sec<10 ? '0' :''}${sec}`
 }
-
-// Call startCountdown to initiate the countdown timer
-startCountdown();
-
-function displayTime(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-
-  const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-  timeH.innerHTML = display;
-}
-
-function endTime() {
-  timeH.innerHTML = 'TIME FOR A BREAK!!';
-  // Redirect to a website
-  window.location.href = 'https://sites.google.com/view/unblockgames-hackuta/home';
-}
-
-try {
-  // Code that may throw an error
-  // For example, accessing an undefined variable or calling a function with incorrect arguments
-} catch (error) {
-  // Handle the error
-  console.error("An error occurred:", error);
+function endTime(){
+    timeH.innerHTML = 'TIME FOR A BREAK!!'
+    window.location="https://sites.google.com/view/unblockgames-hackuta/home";
 }
